8984e3b8ce32f81a0df9d3795c2674aeb4ef9f4e
MrHua269 <wangxyper@163.com>
2024-08-30 13:08:58 +0800

    Fix forgotten implementation

--- zutils-based-api/build.gradle.kts
+++ zutils-based-api/build.gradle.kts
@@ -1,3 +1,6 @@
+group = rootProject.group
+version = rootProject.version
+
 dependencies {
     compileOnly("dev.folia:folia-api:1.20.4-R0.1-SNAPSHOT")
 }
--- zutils-plugin/build.gradle.kts
+++ zutils-plugin/build.gradle.kts
@@ -1,3 +1,6 @@
+group = rootProject.group
+version = rootProject.version
+
 dependencies {
     compileOnly("dev.folia:folia-api:1.20.4-R0.1-SNAPSHOT")
     implementation(project(":zutils-based-api"))
@@ -5,4 +8,5 @@ dependencies {
     implementation(project(":nms:NMS_V1202","reobf"))
     implementation(project(":nms:NMS_V1204","reobf"))*/
     implementation(project(":nms:NMS_V1206","reobf"))
+    implementation(project(":nms:NMS_V1211","reobf"))
 }
